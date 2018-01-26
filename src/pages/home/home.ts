b964d00f8023dcf71dd13eb6c0f406d2daf2e3c4
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-01-26 00:34:57 -0200

    desabilitar menu da tela principal

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 
 @IonicPage()
 @Component({
@@ -8,13 +9,21 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu: MenuController) {
 
   }
 
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false); //Desabilita o menu lateral quando for entrar na página.
+  }
+
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true); //Habilita o menu lateral quand for sair da página.
+  }
+
   login() {
-    //this.navCtrl.push('CategoriasPage');
-    this.navCtrl.setRoot('CategoriasPage');
+    //this.navCtrl.push('CategoriasPage'); //Empilha a página e habilita o botão voltar.
+    this.navCtrl.setRoot('CategoriasPage'); //Utiliza a pagina como principal, mostra o botão menu.
   }
 
 }
