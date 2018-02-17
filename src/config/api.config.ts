9f3f8639e2633159d9bd22f139ed07ffb360d4e6
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-02-17 03:10:48 -0200

    imagens da galeria

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "https://backend-meucarrinho.herokuapp.com/",
+    baseUrl: "https://backend-meucarrinho.herokuapp.com",
     bucketBaseUrl : "https://s3-sa-east-1.amazonaws.com/meucarrinho-ionic"
 }
